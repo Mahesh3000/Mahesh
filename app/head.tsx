73767476c17e7e71c7c2e3014c9cad87ff5f9376
Mahesh3000 <mahesh.sivngi@gmail.com>
2025-05-12 14:45:16 -0500

    header changes

---
+++ app/head.tsx
@@ -0,0 +1,10 @@
+// app/head.tsx
+export default function Head() {
+    return (
+        <>
+            <title>Mahesh</title>
+            <meta name="description" content="Professional portfolio showcasing skills, projects, and experience" />
+            <link rel="icon" href="/favicon.ico" />
+        </>
+    );
+}
